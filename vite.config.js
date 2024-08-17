5ca9431711cb34418af954a8ed368cbc3d016c84
brijeshpatolia <bpatolialtc@gmail.com>
2024-08-17 20:39:00 +0530

    Deploy Vite app to GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://brijeshpatolia.github.io/hangman/', // Replace 'repository-name' with your actual repo name
+  base: '/hangman/', // Replace 'repository-name' with your actual repo name
 });
