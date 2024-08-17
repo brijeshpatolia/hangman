01c819855f078aa9dcd9346b5584d49b618022b5
brijeshpatolia <bpatolialtc@gmail.com>
2024-08-17 20:48:02 +0530

    Updates

--- src/components/MaskedText/MaskedText.test.jsx
+++
@@ -1,4 +0,0 @@
-import { getAllCharacters } from './MaskedText';
-
-describe('getAllCharacters', () => {
-});
--- src/components/MaskedText/MaskedTextUtility.jsx
+++
@@ -1,6 +0,0 @@
-export function getAllCharacters(word, usedLetters) {
-    usedLetters = usedLetters.map(letter => letter.toUpperCase());
-    const guessedLetters = new Set(usedLetters);
-    const characters = word.toUpperCase().split('').map((char) => (guessedLetters.has(char) ? char : '_'));
-    return characters.join(' ');
-}  
